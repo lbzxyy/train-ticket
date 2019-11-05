83603a80350fddcad33bee184341d7cf548807ec
leepoo <leepoo2008@163.com>
2019-11-05 19:56:44 +0800

    改造项目成多页面应用

---
+++ src/index/store.js
@@ -0,0 +1,16 @@
+import {
+  createStore,
+  applyMiddleware,
+  combineReducers
+} from 'redux';
+
+import reducers from './reducers';
+import thunk from 'redux-thunk';
+
+export default createStore(
+  combineReducers(reducers),
+  {
+
+  },
+  applyMiddleware(thunk)
+)
