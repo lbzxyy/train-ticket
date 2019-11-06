5f140022c11b89cd3d396eaf7a7370af36ee2193
leepoo <leepoo2008@163.com>
2019-11-06 11:19:44 +0800

    配置火车票页面相关的redux

--- src/index/App.jsx
+++ src/index/App.jsx
@@ -2,10 +2,24 @@ import React from 'react';
 import { connect } from 'react-redux';
 import './App.css';
 
+import Header from '../common/Header.jsx';
+import DepartDate from './DepartDate.jsx';
+import HighSpeed from './HighSpeed.jsx';
+import Journey from './Journey.jsx';
+import Submit from './Submit.jsx';
+
 function App() {
   return (
     <div className="App">
-     
+      <div className="header-wrapper">
+              <Header title="火车票" />
+      </div>
+      <form action="./query.html" className="form">
+          <Journey />
+          <DepartDate />
+          <HighSpeed  />
+          <Submit />
+      </form>
     </div>
   );
 }
---
+++ src/index/actions.js
@@ -0,0 +1,112 @@
+export const ACTION_SET_FROM = 'SET_FROM';
+export const ACTION_SET_TO = 'SET_TO';
+export const ACTION_SET_IS_CITY_SELECTOR_VISIBLE =
+    'SET_IS_CITY_SELECTOR_VISIBLE';
+export const ACTION_SET_CURRENT_SELECTING_LEFT_CITY =
+    'SET_CURRENT_SELECTING_LEFT_CITY';
+export const ACTION_SET_CITY_DATA = 'SET_CITY_DATA';
+export const ACTION_SET_IS_LOADING_CITY_DATA = 'SET_IS_LOADING_CITY_DATA';
+export const ACTION_SET_IS_DATE_SELECTOR_VISIBLE =
+    'SET_IS_DATE_SELECTOR_VISIBLE';
+export const ACTION_SET_HIGH_SPEED = 'SET_HIGH_SPEED';
+export const ACTION_SET_DEPART_DATE = 'SET_DEPART_DATE';
+// 出发站
+export function setFrom(from) {
+  return {
+    type: ACTION_SET_FROM,
+    payload: from
+  }
+}
+// 抵达站
+export function setTo(to) {
+  return {
+    type: ACTION_SET_TO,
+    payload: to
+  }
+}
+// 节流 是否正在加载请求
+export function setIsLoadingCityData(isLoadingCityData) {
+  return {
+      type: ACTION_SET_IS_LOADING_CITY_DATA,
+      payload: isLoadingCityData,
+  };
+}
+// 设置城市数据
+export function setCityData(cityDate) {
+  return {
+      type: ACTION_SET_CITY_DATA,
+      payload: cityDate,
+  };
+}
+// 切换是否选择只查高铁
+export function toggleHighSpeed() {
+  return (dispatch, getState) => {
+    const { highSpeed } = getState()
+    dispatch({
+      type: ACTION_SET_HIGH_SPEED,
+      payload: !highSpeed
+    })
+  }
+}
+// 显示城市弹窗
+export function showCitySelector(currentSelectingLeftCity) {
+  return (dispatch) => {
+    dispatch({
+      type: ACTION_SET_IS_CITY_SELECTOR_VISIBLE,
+      payload: true
+    })
+    dispatch({
+      type: ACTION_SET_CURRENT_SELECTING_LEFT_CITY,
+      payload: currentSelectingLeftCity
+    })
+  }
+}
+// 隐藏城市弹窗
+export function hideCitySelector() {
+  return{
+    type: ACTION_SET_IS_CITY_SELECTOR_VISIBLE,
+    payload: false
+  }
+}
+// 回显选择哪个城市到那边
+export function setSelectedCity(city) {
+  return (dispatch, getState) => {
+    const { currentSelectingLeftCity } = getState
+    if(currentSelectingLeftCity) {
+      dispatch(setFrom(city))
+    }else{
+      dispatch(setTo(city))
+    }
+  }
+}
+// 显示日前组件
+export function showDateSelector() {
+  return {
+    type: ACTION_SET_IS_DATE_SELECTOR_VISIBLE,
+    payload: true
+  }
+}
+// 隐藏日前组件
+export function hideDateSelector() {
+  return {
+    type: ACTION_SET_IS_DATE_SELECTOR_VISIBLE,
+    payload: false
+  }
+}
+// 切换出发站和抵达站
+export function exchangeFromTo() {
+  return (dispatch, getState) => {
+    const { from, to } = getState()
+    dispatch(setFrom(to))
+    dispatch(setTo(from))
+  }
+}
+// 出发日前
+export function setDepartDate(departDate) {
+  return {
+    type: ACTION_SET_DEPART_DATE,
+    payload: departDate
+  }
+}
+
+
